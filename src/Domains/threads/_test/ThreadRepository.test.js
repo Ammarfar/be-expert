d4192db0ca8c8156a21e2275e878bad2a21c0d67
ammar <ammarfarghani@gmail.com>
2024-02-12 14:02:19 +0700

    feat: add new domain test

--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -8,4 +8,20 @@ describe('ThreadRepository interface', () => {
     // Action and Assert
     await expect(threadRepository.addThread({})).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
+
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action and Assert
+    await expect(threadRepository.verifyExistenceById('thread-123')).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action and Assert
+    await expect(threadRepository.getWithCommentsById('thread-123')).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
 });
